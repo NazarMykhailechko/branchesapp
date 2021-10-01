dede4d77c23d5daf8c7e75f54b17b1b9ecf9c3e6
Михайлечко Назар <mykhailechko@accordbank.com.ua>
2021-10-01 07:38:58 +0300

    add delta js

--- src/main/java/branchapp/Service/ShedulerService.java
+++ src/main/java/branchapp/Service/ShedulerService.java
@@ -30,8 +30,9 @@ public class ShedulerService {
 
       @Async
       //@Scheduled(cron = "*/60 * * * * *")
+      //@Scheduled(cron = "0 */15 * ? * *")
       //@Scheduled(cron = "0 0 1 * * ?")
-      @Scheduled(cron="0 0 1 1 * ?")
+      @Scheduled(cron="0 0 8 1 * ?")
     public void refreshData() throws IOException {
           URL url = new URL("https://bank.gov.ua/NBU_BankInfo/get_dptlist?json");
 
